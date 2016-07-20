a3880916371c822ae16fdb1763f7702d813bc1ac
inbal16@meet.mit.edu <inbal16@meet.mit.edu>
2016-07-20 11:38:41 +0300

    added submit page,updated py app

--- nodep.py
+++ nodep.py
@@ -6,36 +6,41 @@ app = Flask(__name__)
 #Homepage
 @app.route('/')
 def homepage():
+	pass 
 	return render_template(#here we put the homepage.html)
 
 #signup page
 
 @app.route('/about')
 def about():
+	pass
 	return render_template(#here we put about.html)
 
 #signin page
 
 @app.route('/signin')
 def signin():
+	pass
 	return render_template(#here we put signin.html)
 
 #submit page
 
 @app.route('/submit')
 def submit():
-	return render_template(#here we put submit.html)
+	return render_template('submit.html')
 
 #stories page
 
 @app.route('/stories')
 def stories():
+	pass
 	return render_template(#here we put stories)
 
 
 #a single story page
 @app.route('story/<id>')
 def storyid(id):
+	pass
 	return render_template(#singlestory.html, id=id)
 
 #comment page (sending a comment)
@@ -48,6 +53,7 @@ def stories():
 #error page (404)
 @app.errorhandler(404)
 def page_not_found(e):
+	pass
 	return render_template(#'404.html')
 
 if __name__ = '__main__':
