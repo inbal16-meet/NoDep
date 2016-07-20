64a81d8f9e8d10b84a282d45aabcf95c82a0813e
inbal16@meet.mit.edu <inbal16@meet.mit.edu>
2016-07-20 10:44:01 +0300

    finished py (no templates)

--- nodep.py
+++ nodep.py
@@ -1,10 +1,12 @@
 
 from flask import Flask, render_template
+app = Flask(__name__)
+
 
 #Homepage
 @app.route('/')
 def homepage():
-	return render_template('#here we put the homepage.html')
+	return render_template(#here we put the homepage.html)
 
 #signup page
 
@@ -20,15 +22,33 @@ def signin():
 
 #submit page
 
+@app.route('/submit')
+def submit():
+	return render_template(#here we put submit.html)
 
 #stories page
 
+@app.route('/stories')
+def stories():
+	return render_template(#here we put stories)
 
-#comment page
 
+#a single story page
+@app.route('story/<id>')
+def storyid(id):
+	return render_template(#singlestory.html, id=id)
+
+#comment page (sending a comment)
+
+
+@app.route('/comment')
+def stories():
+	return render_template(#comment.html)
 
 #error page (404)
+@app.errorhandler(404)
+def page_not_found(e):
+	return render_template(#'404.html')
 
-
-if __name__ = '__main__'
-app.run(debug=True)
+if __name__ = '__main__':
+	app.run(debug=True)
